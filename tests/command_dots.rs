d6e1fd83c722c8b5d322c3246efb0b7e6b104323
Michael Mullins <michael@webdesserts.com>
2021-12-27 13:27:21 -0600

    Add --version test & cleanup version references

--- tests/command_dots.rs
+++ tests/command_dots.rs
@@ -1,5 +1,6 @@
 mod command_dots {
     use assert_cmd::Command;
+    use clap::crate_version;
     use test_utils::{AssertableOutput, TestResult};
 
     #[test]
@@ -7,6 +8,29 @@ mod command_dots {
         let mut cmd = Command::cargo_bin("dots")?;
         let output = cmd.output()?;
         let expected = include_str!("output/usage.out");
+
+        output.assert_success().assert_stdout_eq(expected);
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_print_help_if_the_help_command_is_passed() -> TestResult {
+        let mut cmd = Command::cargo_bin("dots")?;
+        let output = cmd.arg("--help").output()?;
+        let expected = format!(include_str!("output/help.out"), VERSION = crate_version!());
+
+        output.assert_success().assert_stdout_eq(expected);
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_print_the_version_if_the_version_flag_is_passed() -> TestResult {
+        let mut cmd = Command::cargo_bin("dots")?;
+        let output = cmd.arg("--version").output()?;
+        let expected = format!(
+            include_str!("output/version.out"),
+            VERSION = crate_version!()
+        );
 
         output.assert_success().assert_stdout_eq(expected);
         Ok(())
--- tests/subcommand_help.rs
+++ tests/subcommand_help.rs
@@ -1,12 +1,13 @@
 mod subcommand_help {
     use assert_cmd::Command;
+    use clap::crate_version;
     use test_utils::{AssertableOutput, TestResult};
 
     #[test]
     fn it_should_print_help() -> TestResult {
         let mut cmd = Command::cargo_bin("dots")?;
         let output = cmd.arg("help").output()?;
-        let expected = include_str!("output/help.out");
+        let expected = format!(include_str!("output/help.out"), VERSION = crate_version!());
 
         output.assert_success().assert_stdout_eq(expected);
         Ok(())
