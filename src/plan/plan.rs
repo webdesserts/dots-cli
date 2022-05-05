7be1dfc6e83777327060f9cae7894941c0cf321b
Michael Mullins <michael@webdesserts.com>
2022-05-05 00:00:30 +0000

    Remove outdated TODO list

--- src/plan/plan.rs
+++ src/plan/plan.rs
@@ -12,42 +12,6 @@ use utils::stylize::Stylable;
 
 use super::resolve::{ResolveIssue, ResolveIssueLevel};
 
-/*
-## TODOs
-
-- [ ] rework plan to use new resolve structure
-- [ ] Figure out what to do with the Footprint
-- [ ] Figure out how the footprint should be stored and plan a way get all the way
-      from a LinkRequest to a Footprint
-- [ ] Figure out if a "root" directory should be a part of Anchor::Source
-- [x] Figure out a way to make all of this testable, this is getting out of hand
-    - [x] Make .dots path configurable (probably moved to tmp somewhere)
-    - [x] Make "home directory" configurable (so we aren't still linking relative to it)
-- [x] Figure out how to structure, store, and handle Request Errors & Warnings
-- [x] Figure out how to convert LinkRequests into a flat array of actions
-- [x] Figure out how to continue gathering errors when something errors out.
-
-## What's in a link?
-A link needs to be used to make the actual symlink
-A link needs to be printed out
-A link needs to print out differently based on errors & warnings
-A link needs to be stored in a footprint
-
-## Errors & Warnings
-There are two types of errors:
-Errors resolving the source of a link
-Errors resolving the destination of a link
-
-Warnings – issues that we can resolve, but should require user confirmation.
-The install should still be resumable once a warning is confirmed.
-(e.g Are you sure you want to overwrite this directory?)
-
-Errors – issues that we can't control or issues that need to be resolved by
-the user. Errors should stop the install in its tracks.
-
-We always want to return an Array of Errors & Warnings even if it's empty
-*/
-
 mod styles {
     use utils::{style, stylize::Style};
 
