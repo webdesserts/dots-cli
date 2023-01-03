18630d547aeabf4215a7e17995bd35b202942446
Michael Mullins <michael@webdesserts.com>
2023-01-03 16:08:51 -0600

    fix #46

--- src/fs_manager.rs
+++ src/fs_manager.rs
@@ -33,35 +33,40 @@ impl FSManager {
      * 3. removes the symlink for footprint links that DO NOT have corresponding links in any Dot.toml
      */
     pub fn clean(&mut self, valid_links: &Vec<Link>, env: &Environment) -> Result<()> {
-        let mut next_footprint = self.footprint.clone();
+        let original_footprint = self.footprint.clone();
         debug!("VALID LINKS");
         for link in valid_links {
             debug!("  {link:?}")
         }
         debug!("FOOTPRINT LINKS");
-        for link in &self.footprint.links {
+        for link in &original_footprint.links {
             let symlink_exists = link.dest.path.is_symlink();
             debug!("  {link:?}");
             debug!("    link is on fs: {}", link.exists());
             debug!("    link is in dots: {}", valid_links.contains(link));
             if !symlink_exists {
-                next_footprint.links.remove(link);
                 debug!("    no symlink detected, removing footprint link");
+                self.remove_footprint_link(link)?;
             } else if !link.exists() {
                 debug!("    symlink detected, but pointing to wrong dest, removing footprint link");
-                next_footprint.links.remove(link);
+                self.remove_footprint_link(link)?;
             } else if !link.src.path.starts_with(env.root()) {
                 debug!("    symlink exists, but source is outside of dots dir, removing footprint link");
-                next_footprint.links.remove(link);
+                self.remove_footprint_link(link)?;
             } else if !valid_links.contains(link) {
-                self.remove_symlink(&link.src.path, &link.dest.path)?;
-                next_footprint.links.remove(link);
                 debug!("    link is on fs but is no longer present in dot files, removing symlink & footprint link");
+                self.remove_symlink(&link.src.path, &link.dest.path)?;
+                self.remove_footprint_link(link)?;
             } else {
                 debug!("    leaving link alone")
             }
         }
-        self.footprint = next_footprint;
+        Ok(())
+    }
+
+    /** Removes a link from the footprint file */
+    fn remove_footprint_link(&mut self, link: &Link) -> Result<()> {
+        self.footprint.links.remove(link);
         self.save_footprint()?;
         Ok(())
     }
